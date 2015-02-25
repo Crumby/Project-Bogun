849f52ef5cf12aa40b16307e8d59fc347072d3e7
Crumby <Crumby@users.noreply.github.com>
2015-02-25 19:53:12 +0100

    First test subject

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+	int healt;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
