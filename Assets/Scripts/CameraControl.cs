ee91ffd25fe88f2ef81402eb378698f90a25c603
Crumby <Crumby@users.noreply.github.com>
2015-03-22 20:36:29 +0100

    Test terrain V0.01

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 translation = Vector3.zero;
+
+		translation += new Vector3 (-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+
+		camera.transform.position += 100 * translation * Time.deltaTime;
+	}
+}
