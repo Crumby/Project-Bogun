c442fca117dd5a75f5debd6822498b7ab72e83bf
Agroforte <agroforte@users.noreply.github.com>
2015-07-22 19:41:30 +0200

    script for unit movement movement is straight, unit goes through after "unit" and "destination" is set, set "go" to true to initiate movement script does not set "go" to false (stop movement) by itself , so reaching destination must be tested

---
+++ Assets/Scripts/UnitToPointMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointToPointMovement : MonoBehaviour {
+
+	public bool go = false;
+	public GameObject unit;
+	public Vector3 destination;
+	public float speed = 2;
+
+	// Update is called once per frame
+	void Update () {
+		if (go) {
+			unit.transform.rotation = Quaternion.LookRotation (unit.transform.position - destination, Vector3.up);
+			unit.transform.position = Vector3.MoveTowards (unit.transform.position, destination, speed * Time.deltaTime);
+		}
+	}
+}
